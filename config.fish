f0c8ebb382eb94c507bd1269fa95a1a5e8fa0361
nhaef <me@nico.icu>
2024-02-12 12:18:48 +0100

    config

---
+++ config.fish
@@ -0,0 +1,5 @@
+set -gx GPG_TTY (tty)
+load_nvm > /dev/stderr
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,6 @@
+function fish_prompt
+    set_color B8F
+    echo -n (prompt_pwd)
+    set_color normal
+    echo -n ' # '
+end
