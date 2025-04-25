410b2d9d09e7c5a0c21def3babd46e548cf6f60a
nhaef <me@nico.icu>
2025-04-25 17:00:01 +0200

    refactor fish_prompt, fish_greeting

---
+++ functions/fish_greeting.fish
@@ -0,0 +1,3 @@
+function fish_greeting
+    echo Hi, (set_color bryellow)$USER(set_color normal)! Running fish (set_color purple)$version(set_color normal) on (set_color green)$hostname(set_color normal).
+end
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,6 +1,6 @@
 function fish_prompt
     set_color B8F
-    echo -n (prompt_pwd)
+    echo -n (prompt_pwd -D 3)
     set_color normal
     echo -n ' # '
 end
